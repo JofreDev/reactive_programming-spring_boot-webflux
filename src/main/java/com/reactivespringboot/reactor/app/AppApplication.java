f92c3d09677531a7c2b35b594dcec9467be6bb6e
Jofre Dev <gavidiajofreeduardo@gmail.com>
2024-04-05 13:53:33 -0500

    errors fixed

--- src/main/java/com/reactivespringboot/reactor/app/AppApplication.java
+++ src/main/java/com/reactivespringboot/reactor/app/AppApplication.java
@@ -1,25 +1,8 @@
 package com.reactivespringboot.reactor.app;
 
-import ch.qos.logback.core.spi.PreSerializationTransformer;
-import com.reactivespringboot.reactor.app.model.Comentarios;
-import com.reactivespringboot.reactor.app.model.Usuario;
-import com.reactivespringboot.reactor.app.model.UsuarioComentarios;
-import org.reactivestreams.Subscriber;
-import org.reactivestreams.Subscription;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
-
-import java.lang.management.MemoryNotificationInfo;
-import java.time.Duration;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Timer;
-import java.util.TimerTask;
 
 @SpringBootApplication
 public class AppApplication {
